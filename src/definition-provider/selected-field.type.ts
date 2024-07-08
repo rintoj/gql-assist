503d1e4f244b5f2fd060d271fd192772b4c88d10
Rinto Jose <rintoj@gmail.com>
2024-07-08 17:43:46 +0530

    refactor: selected field and isInRange functions

---
+++ src/definition-provider/selected-field.type.ts
@@ -0,0 +1,4 @@
+export interface SelectedField {
+  parent: string
+  name: string
+}
--- src/position/is-position-within-range.ts
+++ src/position/is-position-within-range.ts
@@ -1,18 +1,29 @@
+import * as gql from 'graphql'
 import { Position, Range } from '../diff'
+import { getGQLNodeRange } from '../gql'
 
 export function isPositionWithInRange(position: Position, range: Range, includeEdges = false) {
   if (position.line > range.start.line && position.line < range.end.line) {
     return true
   }
 
-  if (position.line == range.start.line) {
+  if (position.line === range.start.line && position.line === range.end.line) {
+    if (includeEdges) {
+      return (
+        position.character >= range.start.character && position.character <= range.end.character
+      )
+    } else {
+      return position.character > range.start.character && position.character < range.end.character
+    }
+  }
+  if (position.line === range.start.line) {
     if (includeEdges) {
       return position.character >= range.start.character
     } else {
       return position.character > range.start.character
     }
   }
-  if (position.line == range.end.line) {
+  if (position.line === range.end.line) {
     if (includeEdges) {
       return position.character <= range.end.character
     } else {
@@ -20,4 +31,14 @@ export function isPositionWithInRange(position: Position, range: Range, includeE
     }
   }
   return false
+}
+
+export function isInRange(
+  node: gql.ASTNode,
+  position: Position,
+  offset?: Position,
+  includeEdges = true,
+) {
+  const nodeRange = getGQLNodeRange(node, offset)
+  return isPositionWithInRange(position, nodeRange, includeEdges)
 }
