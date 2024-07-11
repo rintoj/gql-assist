ee80c3994b5c19f3b5ebbf2a0879eaeda0373f6d
Rinto Jose <rintoj@gmail.com>
2024-07-11 20:36:45 +0530

    fix: Should respect the name set by developer for the hook

--- src/ts/get-gql-query-variable.ts
+++ src/ts/get-gql-query-variable.ts
@@ -1,16 +1,30 @@
 import ts from 'typescript'
 import { findNode } from './find-node'
 
+const names = ['gql', 'graphql']
+
 export function getGraphQLQueryVariable(sourceFile: ts.SourceFile) {
   return findNode(
     sourceFile,
     node =>
       ts.isVariableDeclaration(node) &&
       !!node.initializer &&
-      ((ts.isTaggedTemplateExpression(node.initializer) &&
-        ts.isIdentifier(node.initializer.tag) &&
-        node.initializer.tag.escapedText === 'gql') ||
-        ts.isNoSubstitutionTemplateLiteral(node.initializer) ||
-        ts.isStringLiteral(node.initializer)),
+      ts.isTaggedTemplateExpression(node.initializer) &&
+      ts.isIdentifier(node.initializer.tag) &&
+      names.includes(node.initializer.tag.escapedText ?? ''),
   ) as ts.VariableDeclaration
+}
+
+export function getExistingHookName(sourceFile: ts.SourceFile) {
+  const hook = findNode<ts.FunctionDeclaration>(
+    sourceFile,
+    node =>
+      ts.isFunctionDeclaration(node) &&
+      !!node.name &&
+      ts.isIdentifier(node.name) &&
+      !!node.name.text &&
+      node.name.text.startsWith('use'),
+  )
+  if (!hook || !hook.name || !ts.isIdentifier(hook.name)) return
+  return hook.name.text
 }
